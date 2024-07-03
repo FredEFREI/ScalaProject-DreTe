77d5109f1cdf20ee8ac4834b452deff5b48ccf2e
Rockmate77 <52208570+Rockmate77@users.noreply.github.com>
2024-07-03 11:07:21 +0200

    Première interaction

--- APP/src/main/scala/Main.scala
+++ APP/src/main/scala/Main.scala
@@ -1,6 +1,19 @@
 import zio.*
-import zio.Console.printLine
+import zio.Console.*
+import java.io.IOException
+import Utils.*
 
 object Main extends ZIOAppDefault:
+
   override def run: ZIO[Environment & ZIOAppArgs & Scope, Any, Any] =
-    printLine("Welcome to your first ZIO app!")
+    for
+      _ <- printLine("Welcome to our graph app!")
+      option <- readLine("Choose an option:\n1: Load a graph\n2: Exit\n")
+      _ <- option match
+        case "1" => loadFile
+        case "2" => printLine("Exiting...").as(ExitCode.success)
+        case _ => printLine("Invalid option. Please try again.").as(ExitCode.success)
+    yield ExitCode.success
+
+
+
