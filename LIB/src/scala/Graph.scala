39f7c37959b25feeb943fa5b444eb33733446cda
FredEFREI <116546099+FredEFREI@users.noreply.github.com>
2024-07-03 12:04:31 +0200

    Feature: add methode and simplified implementation

--- LIB/src/scala/Graph.scala
+++ LIB/src/scala/Graph.scala
@@ -1,9 +1,16 @@
 // Value of node is generic
 trait Graph[A]:
   def nodes: List[A]
-  def edges: List[(Option[Int], (A , A))]
+  def edges: List[A -> A]
 
-case class GraphDirected[A](nodes: List[A], edges: List[(Option[Int], (A , A))]) extends Graph[A]
-
+case class GraphDirected[A](nodes: List[A], edges: List[(A , A)]) extends Graph[A]:
+  def getNodes:List[A] = nodes
+  def getEdges:List[A -> A] = edges
+  def getNeighbours(node: A):List[A] =
+    for {
+        edge <- edges
+        (from , to) = edge
+        if from == node
+      } yield to 
 end GraphDirected
 
