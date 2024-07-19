5825adac6a236e7241cb4efab70ae70dc636c087
Juxyo <94303202+Juxyo@users.noreply.github.com>
2024-07-19 16:28:18 +0200

    Update automated tests

--- LIB/src/test/scala/lib/testsuiteDirectedWeighted.scala
+++ LIB/src/test/scala/lib/testsuiteDirectedWeighted.scala
@@ -9,12 +9,14 @@ class TestSuite extends AnyFlatSpec {
   it should "be initialized" in {
     val graph = GraphDirectedWeighted[Int](List(), List())
     assert(graph == GraphDirectedWeighted[Int](List(), List()))
+    assert(graph != null)
   }
 
   it should "get the list of nodes" in {
     val graph = GraphDirectedWeighted[Int](List(2, 6, 7), List())
-    val listOfNode = graph.getNodes
-    assert(listOfNode == List(2, 6, 7))
+    assert(graph.getNodes == List(2, 6, 7))
+    val graph1 = GraphDirectedWeighted(List(), List())
+    assert(graph1.getNodes == List())
   }
 
   it should "add an Node" in {
@@ -35,6 +37,8 @@ class TestSuite extends AnyFlatSpec {
   it should "get the list of Edges" in {
     val graph = GraphDirectedWeighted[Int](List(2, 4), List(((2, 4), 6)))
     assert(graph.getEdges == List(((2, 4), 6)))
+    val graph1 = GraphDirectedWeighted(List(), List())
+    assert(graph1.getEdges == List())
   }
 
   it should "add an Edge" in {
@@ -47,8 +51,9 @@ class TestSuite extends AnyFlatSpec {
 
   it should "remove an Edge" in {
     val graph = GraphDirectedWeighted[Int](List(2, 4), List(((2, 4), 6)))
-    val graph_edge = graph.removeEdge(2,4)
+    val graph_edge = graph.removeEdge(2, 4)
     assert(graph.getEdges == List(((2, 4), 6)))
+    assert(graph.removeEdge(2, 4) == GraphDirectedWeighted[Int](List(2, 4), List()))
   }
 
   it should "remove all edges linked to a removed node" in {
@@ -64,14 +69,16 @@ class TestSuite extends AnyFlatSpec {
     assert(graph.getNeighbours(3) == List(2))
     assert(graph.getNeighbours(1) == List(2, 3, 6))
     assert(graph.addNode(9).getNeighbours(9) == List())
+    assert(graph.getNeighbours(10) == List())
+    assert(graph.removeNode(2).getNeighbours(3) == List())
   }
 
   it should "be able to get all nodes that have an edge leading to a selected node" in {
     val graph = GraphDirectedWeighted(List(1, 2, 3, 5, 4, 10, 6, 7), List(((1, 2), 1), ((3, 2), 3), ((1, 3), 4), ((4, 3), 8), ((4, 5), 5), ((5, 2), 2), ((1, 6), 9)))
     assert(graph.getPrevious(3) == List(1, 4))
     assert(graph.getPrevious(1) == List())
+    assert(graph.getPrevious(5) == List(4))
   }
-
 
   it should "transform into a 'Dot' format" in {
     val graph = GraphDirectedWeighted(List(1, 2, 3, 5, 4, 10, 6, 7),List(((1,2),1), ((3,2),3), ((1,3),4), ((4,3),8), ((4,5),5), ((5,2),2), ((1,6),9)))
@@ -83,7 +90,6 @@ class TestSuite extends AnyFlatSpec {
   it should "compute the dfs algorithm" in {
     val graph = GraphDirectedWeighted(List(1, 2, 3, 5, 4, 10, 6, 7), List(((1, 2), 1), ((3, 2), 3), ((1, 3), 4), ((4, 3), 8), ((4, 5), 5), ((5, 2), 2), ((1, 6), 9)))
     assert(graph.dfs(1) == List(1, 2, 3, 6))
-
     val graph2 = GraphDirectedWeighted(List(1), List())
     assert(graph2.dfs(1) == List(1))
   }
@@ -91,11 +97,15 @@ class TestSuite extends AnyFlatSpec {
   it should "compute the bfs algorithm" in {
     val graph = GraphDirectedWeighted(List(1, 2, 3, 5, 4, 10, 6, 7), List(((1, 2), 1), ((3, 2), 3), ((1, 3), 4), ((4, 3), 8), ((4, 5), 5), ((5, 2), 2), ((1, 6), 9)))
     assert(graph.bfs(1) == List(List(1), List(2, 3, 6)))
+    val graph1 = GraphDirectedWeighted(List(1), List())
+    assert(graph1.bfs(1) == List(List(1)))
   }
 
   it should "sort nodes using topological sorting algorithm" in {
     val graph = GraphDirectedWeighted(List(1, 2, 3, 5, 4, 10, 6, 7), List(((1, 2), 1), ((3, 2), 3), ((1, 3), 4), ((4, 3), 8), ((4, 5), 5), ((5, 2), 2), ((1, 6), 9)))
     assert(graph.topologicalSort() == List(10, 7, 1, 4, 5, 6, 3, 2))
+    val graph1 = GraphDirectedWeighted(List(),List())
+    assert(graph1.topologicalSort() == List())
   }
 
   it should "detect if a graph has a cycle in it" in {
